0f16e168c6c703aca21b0249b3e3aa36ea6b2c5d
Robin Roy <robinroy.work@gmail.com>
2023-12-04 17:37:15 +0530

    aa

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.markdown("### An Aggregation portal for various helpline numbers (Cyclone Michaung)")
+
+st.markdown("""
+    ### Chennai corportation (tree fall/ water logging)
+            1913
+    ### Electricity
+            +91 94987 94987
+    ### Metro Water & Sewage
+            044 4567 4567
+        1916
+    
+    ### Snake Rescue
+            044 2220 0335
+    
+    ### LPG Gas Leakage
+            1906
+    
+    ### Chennai Metro Rail
+            1860 425 1515
+    
+    ### Blue Cross (Animal Resuce)
+            bit.ly/bluecross-help
+    
+    ### Women helpline
+            181
+    
+    ### Childline
+            1098
+    
+    ### Police
+            100
+    
+    ### Traffic Police
+            103
+    
+    ### Ambulance
+            108
+""")
